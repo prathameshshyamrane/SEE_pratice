bcd050ec5ee7fe7f873a746842533aafdabbc3d7
prathameshshyamrane <psr261004@gmail.com>
2024-04-29 03:23:31 +0530

    commit

---
+++ SEE_practice/src/practice/AddNumbers.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Close the scanner to prevent resource leaks
+        scanner.close();
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("Sum: " + sum);
+    }
+}
